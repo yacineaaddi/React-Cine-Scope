e5669f8b050e7b416d4fcaf01652cb4203898fb2
yacine <yacineaaddi@gmail.com>
2025-09-02 19:18:10 +0100

    Start working on the starRating Component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
+import StarRating from "./StarRating.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* } <App /> */}
+    <StarRating maxRating={10} />
+    <StarRating maxRating={7} />
+    <StarRating />
   </React.StrictMode>
 );
