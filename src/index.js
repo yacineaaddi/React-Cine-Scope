f74fe7f48c3971a60dbeb5bb3a64abb858e466f1
yacine <yacineaaddi@gmail.com>
2025-09-05 10:17:52 +0100

    Improving Reusability With Props

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* } <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={10}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaulRating={3}
+    />
   </React.StrictMode>
 );
