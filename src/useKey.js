bb2f475340800f3bf529b2414aa4a9fb4e974f23
yacine <yacineaaddi@gmail.com>
2025-10-13 00:00:00 +0100

    Fix a small bug

--- src/useKey.js
+++ src/useKey.js
@@ -4,9 +4,8 @@ export default function useKey(key, action) {
   useEffect(
     function () {
       function callback(e) {
-        if (e.code === key.toLowerCase()) {
+        if (e.code.toLowerCase() === key.toLowerCase()) {
           action();
-          console.log(key);
         }
       }
       document.addEventListener("keydown", callback);
